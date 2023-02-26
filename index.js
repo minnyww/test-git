e5339e3e242d1b3fd4b1a4e3aba9e5fdedb9e1f3
mnswjrs <manassawee@cleverse.com>
2023-02-26 14:53:04 +0700

    mn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const name = "Pang asdasdasdasd asd";
 const version = 12312312 - 021 - 10233121221312331232;
 const pang = "12312391adsa2093801982312";
 const min = "aasdadasd";
-const branch = "veasdasdasdrsion 1"
+const branch = "veasdasdasdrsion 1";
