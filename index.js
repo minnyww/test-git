e83b316e706a54324ba87873998c3b486c95a91c
minnyww <minrrverse@gmail.com>
2023-02-26 14:42:57 +0700

    adasd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const name = "Pang asdasdasdasd asd";
-const version = 123123123121221312331232;
-const pang = "123123912093801982312";
-const min = "asdasdasd";
-const branch = "version 1"
+const version = 12312312 - 021 - 10233121221312331232;
+const pang = "12312391adsa2093801982312";
+const min = "aasdadasd";
+const branch = "veasdasdasdrsion 1"
